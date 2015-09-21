19a587393fb071128849755024cba5445b1a2b1f
yokoyama takuya <yoko_1020_takuya@yahoo.co.jp>
2015-09-22 03:54:24 +0900

    その6完成。

--- TwitterApp/TimeLineTableViewController.swift
+++ TwitterApp/TimeLineTableViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Social
 import Accounts
+import Alamofire
 
 class TimeLineTableViewController: UITableViewController {
     
@@ -26,6 +27,12 @@ class TimeLineTableViewController: UITableViewController {
             "image":"http://ux-mobile.net/data/public/spartacamp/201509/twitter_icon.png"
         ]
     ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loginTwitter()
+    }
     
     //テーブルの件数を登録
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,6 +61,111 @@ class TimeLineTableViewController: UITableViewController {
     }
     
 
+    @IBAction func tapTweetButton(sender: UIBarButtonItem) {
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            //CancelもしくはPostを押した際に呼ばれ、投稿画面を閉じる処理を行っています。
+            vc.completionHandler = {(result:SLComposeViewControllerResult) -> () in
+                vc.dismissViewControllerAnimated(true, completion:nil)
+            }
+            
+            ////投稿画面の初期値設定
+            //vc.setInitialText("初期テキストを設定できます。")
+            //vc.addURL(NSURL(string:"シェアURLを設定できます。"))
+            self.presentViewController(vc, animated: true, completion: nil)
+            
+        }else{
+            print("******** アカウント未設定エラー")
+        }
+    }
+
     
+    
+    /*
+    *   Twitterのアクセストークンを取得
+    */
+    
+    func loginTwitter(){
+        
+        //Twitterが登録されていないケース
+        if !SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            return
+        }
+        
+        let store = ACAccountStore();
+        let type = store.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        
+        store.requestAccessToAccountsWithType(type, options: nil) { (granted, error) -> Void in
+            
+            if error != nil{
+                return;
+            }
+            
+            if granted == false{
+                //アカウントは登録されているが認証が拒否されたケース
+                return;
+            }
+            
+            let accounts = store.accountsWithAccountType(type);
+            
+            if accounts.count == 0{
+                return;
+            }
+            
+            if let account = accounts[0] as? ACAccount{
+                print("自分のアカウント名：「\(account.username)」\n")
+                
+                //アカウントをメモリに保持
+                self.twitterAccount = account
+                
+                //タイムラインの取得
+                self.downloadTwitterTimeLine()
+            }
+        }
+    }
+    
+    /*
+    *	Twitterのタイムラインを取得する
+    */
+    
+    func downloadTwitterTimeLine(){
+        
+        //自分の投稿一覧は「user_timeline.json」で取得可能
+        let URL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+        let preRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, URL: URL, parameters: nil)
+        preRequest.account = twitterAccount
+        
+        Alamofire.request(preRequest.preparedURLRequest()).responseJSON { _, _, result in
+            
+            if result.isSuccess,
+                let json = result.value as? [[String:AnyObject]]{
+                    
+                    for entry in json{
+                        
+                        if let user = entry["user"] as? [String: AnyObject]
+                            ,let name = user["name"] as? String
+                            ,let imageURLString = user["profile_image_url"] as? String{
+                                
+                                print("ユーザー名：「\(name)」")
+                                print("アイコン画像：「\(imageURLString)」")
+                                
+                        }
+                        
+                        if let text = entry["text"] as? String{
+                            print(text)
+                        }
+                        print("------------------------")
+                    }
+                    
+            }
+            
+        }
+        
+    }
+    
+
 
 }
